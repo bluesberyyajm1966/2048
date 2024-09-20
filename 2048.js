1381735e45a07f386a35cf40043ff7627d2fd54c
bluesberyyajm1966 <149900026+bluesberyyajm1966@users.noreply.github.com>
2024-09-20 13:35:36 -0700

    Update 2048.js

--- 2048.js
+++ 2048.js
@@ -6,10 +6,6 @@ var columns = 4;
 window.onload = function() {
     setGame();
 }
-document.querySelector('.restart-btn').addEventListener('click', function(){
-  window.location.reload();
-  return false;
-});
 
 function setGame() {
     // board = [
